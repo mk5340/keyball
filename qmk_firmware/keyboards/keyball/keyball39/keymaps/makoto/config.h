99e0fd3b83d25a1542a240279c6f4ed7bd23a65d
mk5340 <mk.ak5.a5lkstu319sin@gmail.com>
2025-01-05 17:54:34 +0900

    マウスレイヤーの番号を修正

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/makoto/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/makoto/config.h
@@ -42,7 +42,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DYNAMIC_KEYMAP_LAYER_COUNT 6 // レイヤーの追加
 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE // 有効化
-#define AUTO_MOUSE_DEFAULT_LAYER 2 // 切り替えるマウスレイヤー番号を指定
+#define AUTO_MOUSE_DEFAULT_LAYER 4 // 切り替えるマウスレイヤー番号を指定
 #define AUTO_MOUSE_TIME 500 // マウスが止まってから元のレイヤーに戻るまでの時間(ms)
 
 #define KEYBALL_CPI_DEFAULT 1100 // マウス速度 (default: 500)
