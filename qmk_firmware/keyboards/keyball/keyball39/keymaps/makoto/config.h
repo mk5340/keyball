120a1b6afd6a7cf90972f87c23c91d075f817b11
mk5340 <mk.ak5.a5lkstu319sin@gmail.com>
2025-01-18 15:50:30 +0900

    AMLの切り替え時間

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/makoto/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/makoto/config.h
@@ -38,10 +38,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAPPING_TERM 180 // ミリ秒　#キー長押し時間の時間修正
 #define DYNAMIC_KEYMAP_LAYER_COUNT 6 // レイヤーの追加
-
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 4 // 切り替えるマウスレイヤー番号を指定
-//#define AUTO_MOUSE_TIME 500 // マウスが止まってから元のレイヤーに戻るまでの時間(ms)
+#define AUTO_MOUSE_TIME 500 // マウスが止まってから元のレイヤーに戻るまでの時間(ms)
 
 #define KEYBALL_CPI_DEFAULT 1100 // マウス速度 (default: 500)
 #define KEYBALL_SCROLL_DIV_DEFAULT 5 // スクロール速度 (default: 4)
