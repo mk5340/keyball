f137d194d64db9f73901cfd751ecc857b3ab8096
mk5340 <mk.ak5.a5lkstu319sin@gmail.com>
2025-01-25 22:33:25 +0900

    加速度調整

--- qmk_firmware/keyboards/keyball/lib/keyball/keyball.c
+++ qmk_firmware/keyboards/keyball/lib/keyball/keyball.c
@@ -173,13 +173,13 @@ static void adjust_mouse_speed(keyball_motion_t *m){
     float speed_multiplier = 1.0;
 
     if(movement_size > 60){
-        speed_multiplier = 6.0; //3.0
+        speed_multiplier = 20; //3.0
     }else if (movement_size > 30)
     {
-        speed_multiplier = 3.0; //1.5
+        speed_multiplier = 1.5; //1.5
     }else if (movement_size > 5)
     {
-        speed_multiplier = 2.0; //1.0
+        speed_multiplier = 1.0; //1.0
     }else if (movement_size > 4)
     {
         speed_multiplier = 0.9;
